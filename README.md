4ebedab17e9fb1fc0fb5dc848dc7d9d0f63c4d0e
Boğaçhan Yiğitbaşı <by@concordium.com>
2025-01-06 08:34:19 +0300

    added readme, installed openzeppelin contracts, and created the stablecoin contract

---
+++ README.md
@@ -0,0 +1,22 @@
+1. A stablecoin pegged to 1 USD
+   - Chainlink Pricefeed
+   - Set a function to exchange ETH and BTC -> $
+2. Stability mechanism will be algorithmic -> No mint and
+   burn by centralized entity - People can mint with collateral
+3. Collateral type: Crypto
+   - wETH
+   - wBTC
+
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.js
+```
---
+++ contracts/DecentralizedStablecoin.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20, ERC20Burnable} from "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+/**
+    @title DecentralizeStablecoin
+    @author by
+    Collateral: wETH, wBTC
+    Minting: Algorithmic
+    Stability: using Chainlink
+
+There are 2 main contracts of the project, this is just an ERC20
+governed by DSEngine (DecentralizedStablecoinEngine) contract
+*/
+// we want to burn
+contract DecentralizedStablecoin is ERC20Burnable, Ownable {
+    // errors
+    error DSC_MustBeGreaterThanZero();
+    error DSC_MintTryForZeroAddress();
+    // ERC20Burnable is ERC20 which means we have to use ERC20 constructor as well.
+    constructor() ERC20("Decentralized Stablecoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        if (_amount <= 0) {
+            revert DSC_MustBeGreaterThanZero();
+        }
+        uint256 balance = balanceOf(msg.sender);
+        require(_amount < balance, "Not enough token balance");
+
+        // use the super class to burn
+        super.burn(_amount);
+    }
+
+    // not overriding anything for `mint`
+    // there is no mint function either in parent
+    function mint(
+        address _to,
+        uint256 _amount
+    ) public onlyOwner returns (bool) {
+        if (_amount <= 0) {
+            revert DSC_MustBeGreaterThanZero();
+        }
+        if (_to == address(0)) {
+            revert DSC_MintTryForZeroAddress();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
